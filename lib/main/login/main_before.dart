467f50d03da8b9ccdc7e9f09490584516e569303
Aemiroyoo <sutiyoyulianto2000@gmail.com>
2025-03-07 10:10:47 +0700

    Add initial login screen setup and main application structure

---
+++ lib/main/login/main_before.dart
@@ -0,0 +1,22 @@
+// import 'package:first_project/main/login/login_screen.dart';
+// import 'package:first_project/main/quiz/quiz_screen.dart';
+// // import 'package:first_project/main/main_screen.dart';
+// // import 'package:first_project/views/auth/login_screen.dart';
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   static const appTitle = 'Drawer Demo';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: appTitle,
+//       home: QuizScreen(),
+//     );
+//   }
+// }
